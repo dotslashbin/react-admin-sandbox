57d06bfdbc026d295cc00f8509ee03ec56590d5b
Joshua Fuentes <joshua.alphanova@gmail.com>
2018-12-21 16:14:30 +0800

    Initial implementation on the main App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+/////////////
+// Modules //
+/////////////
+import React from 'react'
+// import { Admin, Resource, ListGuesser } from 'react-admin'
+import { Admin, Resource } from 'react-admin'
+import { PostList } from './components/PostList'
+import jsonServerProvider from 'ra-data-json-server'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import { UserList } from './components/users'
 
-export default App;
+
+const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
+
+const App = () => (
+  <Admin dataProvider={ dataProvider }>
+  	
+  	<Resource name="posts" list={ PostList } />
+    <Resource name="users" list={ UserList } />
+  </Admin>
+)
+
+export default App
