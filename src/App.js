a65c0e2d904f5786f3c82c2e1acd9eb748197d4b
JoshuaF <joshuarpf@gmail.com>
2018-12-23 18:44:25 +0800

    Added icons

--- src/App.js
+++ src/App.js
@@ -10,14 +10,20 @@ import jsonServerProvider from 'ra-data-json-server'
 
 import { UserList } from './components/users'
 
+////////////
+// Icons  //
+////////////
+import PostIcon from '@material-ui/icons/Book'
+import UserIcon from '@material-ui/icons/Group'
+
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
 
 const App = () => (
   <Admin dataProvider={ dataProvider }>
   	
-  	<Resource name="posts" list={ PostList } edit={ PostEdit } create={ PostCreate } />
-    <Resource name="users" list={ UserList } />
+  	<Resource name="posts" list={ PostList } edit={ PostEdit } create={ PostCreate } icon={PostIcon}/>
+    <Resource name="users" list={ UserList } icon={UserIcon}/>
 
   </Admin>
 )
