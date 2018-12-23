467fd1e24cbaf51d04b0939aec5649c6eb181ac5
JoshuaF <joshuarpf@gmail.com>
2018-12-23 18:52:52 +0800

    Implmented dashboard

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import React from 'react'
 // import { Admin, Resource, ListGuesser } from 'react-admin'
 import { Admin, Resource } from 'react-admin'
 import { PostList, PostCreate, PostEdit } from './components/posts'
-
 import jsonServerProvider from 'ra-data-json-server'
-
 import { UserList } from './components/users'
+
+import Dashboard from './components/Dashboard'
 
 ////////////
 // Icons  //
@@ -19,6 +19,7 @@ import UserIcon from '@material-ui/icons/Group'
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
 
+/*
 const App = () => (
   <Admin dataProvider={ dataProvider }>
   	
@@ -26,6 +27,12 @@ const App = () => (
     <Resource name="users" list={ UserList } icon={UserIcon}/>
 
   </Admin>
-)
+)*/
 
+const App = () => (
+	<Admin dashboard={ Dashboard } dataProvider={ dataProvider }>
+		<Resource name="posts" list={ PostList } edit={ PostEdit } create={ PostCreate } icon={PostIcon}/>
+    	<Resource name="users" list={ UserList } icon={UserIcon}/>
+	</Admin>
+)
 export default App
---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react' 
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
+
+export default () => (
+	<Card>
+		<CardHeader title="Welcome to your stuff"/>
+		<CardContent>
+			Lorem ipsum velit enim cillum culpa quis ea incididunt aute aliquip.
+		</CardContent>
+	</Card>
+)
