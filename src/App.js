59b1b05f041336a05d080e08ec0a738efde965fa
Joshua Fuentes <joshua.alphanova@gmail.com>
2018-12-26 18:49:33 +0800

    working form but stuck on passing REST to server

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import jsonServerProvider from 'ra-data-json-server'
 import { UserList } from './components/users'
 import authProvider from './components/authProvider'
 import Dashboard from './components/Dashboard'
+
+import { Login } from 'react-admin'
 import MyLoginPage from './components/MyLoginPage'
 
 ////////////
@@ -16,6 +18,14 @@ import MyLoginPage from './components/MyLoginPage'
 ////////////
 import PostIcon from '@material-ui/icons/Book'
 import UserIcon from '@material-ui/icons/Group'
+
+import { withStyles } from '@material-ui/core/styles'
+
+const styles = {
+	formcontianer: {
+		backgroundColor: 'FFFFFF'
+	}
+}
 
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
@@ -30,10 +40,25 @@ const App = () => (
   </Admin>
 )*/
 
+// pre-created
+// const MyLoginPage = () => (
+// 	<div>
+// 		<div>
+// 		<h1>You can change this entire page</h1>
+// 		</div>
+// 		<div>
+// 			<Login backgroundImage="" />
+// 		</div> 
+// 	</div>
+		
+// )
+
 const App = () => (
 	<Admin loginPage={ MyLoginPage } dashboard={ Dashboard } dataProvider={ dataProvider } authProvider={ authProvider }>
 		<Resource name="posts" list={ PostList } edit={ PostEdit } create={ PostCreate } icon={PostIcon}/>
     	<Resource name="users" list={ UserList } icon={UserIcon}/>
 	</Admin>
 )
+
+
 export default App
--- src/components/MyLoginPage.js
+++ src/components/MyLoginPage.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { userLogin } from 'react-admin'
 import { MuiThemeProvider } from '@material-ui/core/styles'
+import { withStyles } from '@material-ui/core/styles';
+
 class MyLoginPage extends Component {
 
 	constructor() {
@@ -33,14 +35,23 @@ class MyLoginPage extends Component {
         this.props.userLogin(credentials);
     }
 
+
+
     render() {
         return (
             <MuiThemeProvider >
-                <form onSubmit={this.submit}>
-                	email: <input type="text" name="email" value={ this.state.email } onChange={ this.onChange } />
-                	password: <input type="password" name="password" value={ this.state.password } onChange={ this.onChange }/>
-                	<input type="submit" />
-                </form>
+            	<div>
+	            	<div>
+
+	            	</div>
+	            	<div>
+	            		<form onSubmit={this.submit}>
+		                	email: <input type="text" name="email" value={ this.state.email } onChange={ this.onChange } />
+		                	password: <input type="password" name="password" value={ this.state.password } onChange={ this.onChange }/>
+		                	<input type="submit" />
+		                </form>
+	            	</div>
+            	</div>
             </MuiThemeProvider>
         );
     }
